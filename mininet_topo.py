131b83a8770a942b17d3dc821e569c11b71fc2ac
mpater59 <mikolajpater123@gmail.com>
2024-03-06 00:09:40 +0100

    Modified mininet_topo.py, added notes.txt

--- mininet_topo.py
+++ mininet_topo.py
@@ -6,114 +6,48 @@ from mininet.node import RemoteController, OVSSwitch
 from mininet.link import TCLink
 from mininet.topolib import Topo
 
-# parser = argparse.ArgumentParser()
-# parser.add_argument('-s', '--spine', dest='spine', default=2, type=int,
-#                     help='Number of spine switches (default 2)')
-# parser.add_argument('-l', '--leaf', dest='leaf', default=4, type=int,
-#                     help='Number of leaf switches (default 4)')
-# parser.add_argument('-ht', '--host', dest='host', default=3, type=int,
-#                     help='Number of hosts (default 3)')
-# args = parser.parse_args()
-#
-# ryu_controller = RemoteController('ryu', ip='127.0.0.1', port=6633)
-# net = Mininet(switch=OVSSwitch, link=TCLink, autoSetMacs=True, autoStaticArp=True)
-#
-# s1 = net.addSwitch('s1')
-#
-# spine_switches = []
-# leaf_switches = []
-# servers = []
-# hosts = []
-# host_links = []
-# server_links = []
-#
-# index = 1
-#
-# for x in range(args.host):
-#     # hosts.append(net.addHost(f'h{x + 1}', ip=f'192.168.{x + 1}.11/24'))
-#     hosts.append(net.addHost(f'h{x + 1}', ip=f'10.0.0.{index}/24'))
-#     index += 1
-#     host_links.append(net.addLink(hosts[x], s1))
-#
-# for x in range(args.spine):
-#     spine_switches.append(net.addSwitch(f's{x + 2}'))
-#     net.addLink(s1, spine_switches[x], bw=10, delay='10ms', loss=0, max_queue_size=10000)
-#
-# for x in range(args.leaf):
-#     leaf_switches.append(net.addSwitch(f's{x + 2 + args.spine}'))
-#     # servers.append(net.addHost(f'h{x + 1 + args.host}', ip=f'10.0.{x + 1}.11/24'))
-#     servers.append(net.addHost(f'h{x + 1 + args.host}', ip=f'10.0.0.{index}/24'))
-#     index += 1
-#     server_links.append(net.addLink(servers[x], leaf_switches[x]))
-#     for spine in spine_switches:
-#         net.addLink(spine, leaf_switches[x], bw=10, delay='10ms', loss=0, max_queue_size=10000)
-#
-# net.addController(ryu_controller)
-#
-# net.start()
-#
-# for host, link in zip(hosts, host_links):
-#     host.setDefaultRoute(intf=link.intf1)
-# for server, link in zip(servers, server_links):
-#     server.setDefaultRoute(intf=link.intf1)
-
 
 class Topology(Topo):
     def build(self):
-        # self.addSwitch('s1', protocols='OpenFlow13')
-        # self.addSwitch('s2', protocols='OpenFlow13')
-        #
-        # self.addHost('h1', ip='10.0.0.1/24')
-        # self.addHost('h2', ip='10.0.0.2/24')
-        #
-        # self.addLink('h1', 's1', bw=10, delay='10ms')
-        # self.addLink('s1', 's2', bw=10, delay='10ms')
-        # self.addLink('s2', 'h2', bw=10, delay='10ms')
-
-        s1 = self.addSwitch('s1')
-
         spine_switches = []
         leaf_switches = []
         servers = []
-        hosts = []
-        host_links = []
         server_links = []
 
-        index = 1
-
-        for x in range(args.host):
-            # hosts.append(net.addHost(f'h{x + 1}', ip=f'192.168.{x + 1}.11/24'))
-            hosts.append(self.addHost(f'h{x + 1}', ip=f'10.0.0.{index}/24'))
-            index += 1
-            host_links.append(self.addLink(hosts[x], s1))
-
         for x in range(args.spine):
-            spine_switches.append(self.addSwitch(f's{x + 2}'))
-            self.addLink(s1, spine_switches[x], bw=10, delay='10ms')
+            spine_switches.append(self.addSwitch(f's{x + 1}'))
 
         for x in range(args.leaf):
-            leaf_switches.append(self.addSwitch(f's{x + 2 + args.spine}'))
-            # servers.append(net.addHost(f'h{x + 1 + args.host}', ip=f'10.0.{x + 1}.11/24'))
-            servers.append(self.addHost(f'h{index}', ip=f'10.0.0.{index}/24'))
-            server_links.append(self.addLink(servers[x], leaf_switches[x]))
-            index += 1
+            leaf_switches.append(self.addSwitch(f's{x + 1 + args.spine}'))
             for spine in spine_switches:
                 self.addLink(spine, leaf_switches[x], bw=10, delay='10ms')
+            servers.append(self.addHost(f'h{2 * x + 1}', ip=f'10.0.0.{x + 1}/24'))
+            server_links.append(self.addLink(servers[2 * x], leaf_switches[x]))
+            servers.append(self.addHost(f'h{2 * x + 2}', ip=f'10.0.1.{x + 1}/24'))
+            server_links.append(self.addLink(servers[2 * x + 1], leaf_switches[x]))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', '--spine', dest='spine', default=2, type=int,
                         help='Number of spine switches (default 2)')
-    parser.add_argument('-l', '--leaf', dest='leaf', default=4, type=int,
-                        help='Number of leaf switches (default 4)')
-    parser.add_argument('-ht', '--host', dest='host', default=3, type=int,
-                        help='Number of hosts (default 3)')
+    parser.add_argument('-l', '--leaf', dest='leaf', default=3, type=int,
+                        help='Number of leaf switches (default 3)')
     args = parser.parse_args()
 
     ryu_controller = RemoteController('ryu', ip='127.0.0.1', port=6633)
     topo = Topology()
-    net = Mininet(switch=OVSSwitch, link=TCLink, topo=topo, controller=ryu_controller)
+    net = Mininet(switch=OVSSwitch, link=TCLink, topo=topo, controller=ryu_controller, autoSetMacs=True)
+
+    for host in net.hosts:
+        host.cmd("sysctl -w net.ipv6.conf.all.disable_ipv6=1")
+        host.cmd("sysctl -w net.ipv6.conf.default.disable_ipv6=1")
+        host.cmd("sysctl -w net.ipv6.conf.lo.disable_ipv6=1")
+
+    for switch in net.switches:
+        switch.cmd("sysctl -w net.ipv6.conf.all.disable_ipv6=1")
+        switch.cmd("sysctl -w net.ipv6.conf.default.disable_ipv6=1")
+        switch.cmd("sysctl -w net.ipv6.conf.lo.disable_ipv6=1")
 
     net.start()
     CLI(net)
