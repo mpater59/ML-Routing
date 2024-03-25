7d618a0af8abdd2419eaf01d1d78a26ac854060a
mpater59 <mikolajpater123@gmail.com>
2024-03-25 16:10:45 +0100

    Added new mininet_topo.py for core network (test)

---
+++ leaf_and_spice_old/mininet_topo.py
@@ -0,0 +1,54 @@
+import argparse
+
+from mininet.net import Mininet
+from mininet.cli import CLI
+from mininet.node import RemoteController, OVSSwitch
+from mininet.link import TCLink
+from mininet.topolib import Topo
+
+
+class Topology(Topo):
+    def build(self):
+        spine_switches = []
+        leaf_switches = []
+        servers = []
+        server_links = []
+
+        for x in range(args.spine):
+            spine_switches.append(self.addSwitch(f's{x + 1}'))
+
+        for x in range(args.leaf):
+            leaf_switches.append(self.addSwitch(f's{x + 1 + args.spine}'))
+            for spine in spine_switches:
+                self.addLink(spine, leaf_switches[x], bw=10, delay='10ms')
+            servers.append(self.addHost(f'h{2 * x + 1}', ip=f'10.0.0.{x + 1}/24'))
+            server_links.append(self.addLink(servers[2 * x], leaf_switches[x]))
+            servers.append(self.addHost(f'h{2 * x + 2}', ip=f'10.0.1.{x + 1}/24'))
+            server_links.append(self.addLink(servers[2 * x + 1], leaf_switches[x]))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--spine', dest='spine', default=2, type=int,
+                        help='Number of spine switches (default 2)')
+    parser.add_argument('-l', '--leaf', dest='leaf', default=3, type=int,
+                        help='Number of leaf switches (default 3)')
+    args = parser.parse_args()
+
+    ryu_controller = RemoteController('ryu', ip='127.0.0.1', port=6633)
+    topo = Topology()
+    net = Mininet(switch=OVSSwitch, link=TCLink, topo=topo, controller=ryu_controller, autoSetMacs=True)
+
+    for host in net.hosts:
+        host.cmd("sysctl -w net.ipv6.conf.all.disable_ipv6=1")
+        host.cmd("sysctl -w net.ipv6.conf.default.disable_ipv6=1")
+        host.cmd("sysctl -w net.ipv6.conf.lo.disable_ipv6=1")
+
+    for switch in net.switches:
+        switch.cmd("sysctl -w net.ipv6.conf.all.disable_ipv6=1")
+        switch.cmd("sysctl -w net.ipv6.conf.default.disable_ipv6=1")
+        switch.cmd("sysctl -w net.ipv6.conf.lo.disable_ipv6=1")
+
+    net.start()
+    CLI(net)
+    net.stop()
--- mininet_topo.py
+++ mininet_topo.py
@@ -9,45 +9,25 @@ from mininet.topolib import Topo
 
 class Topology(Topo):
     def build(self):
-        spine_switches = []
-        leaf_switches = []
-        servers = []
-        server_links = []
+        s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
+        s3 = self.addSwitch('s3')
+        s4 = self.addSwitch('s4')
+        s5 = self.addSwitch('s5')
 
-        for x in range(args.spine):
-            spine_switches.append(self.addSwitch(f's{x + 1}'))
-
-        for x in range(args.leaf):
-            leaf_switches.append(self.addSwitch(f's{x + 1 + args.spine}'))
-            for spine in spine_switches:
-                self.addLink(spine, leaf_switches[x], bw=10, delay='10ms')
-            servers.append(self.addHost(f'h{2 * x + 1}', ip=f'10.0.0.{x + 1}/24'))
-            server_links.append(self.addLink(servers[2 * x], leaf_switches[x]))
-            servers.append(self.addHost(f'h{2 * x + 2}', ip=f'10.0.1.{x + 1}/24'))
-            server_links.append(self.addLink(servers[2 * x + 1], leaf_switches[x]))
+        self.addLink(s1, s2, bw=10, delay='10ms')
+        self.addLink(s1, s3, bw=10, delay='5ms')
+        self.addLink(s1, s4, bw=15, delay='15ms')
+        self.addLink(s3, s4, bw=15, delay='15ms')
+        self.addLink(s4, s2, bw=5, delay='5ms')
+        self.addLink(s3, s5, bw=5, delay='5ms')
+        self.addLink(s5, s4, bw=5, delay='5ms')
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-s', '--spine', dest='spine', default=2, type=int,
-                        help='Number of spine switches (default 2)')
-    parser.add_argument('-l', '--leaf', dest='leaf', default=3, type=int,
-                        help='Number of leaf switches (default 3)')
-    args = parser.parse_args()
-
     ryu_controller = RemoteController('ryu', ip='127.0.0.1', port=6633)
     topo = Topology()
     net = Mininet(switch=OVSSwitch, link=TCLink, topo=topo, controller=ryu_controller, autoSetMacs=True)
-
-    for host in net.hosts:
-        host.cmd("sysctl -w net.ipv6.conf.all.disable_ipv6=1")
-        host.cmd("sysctl -w net.ipv6.conf.default.disable_ipv6=1")
-        host.cmd("sysctl -w net.ipv6.conf.lo.disable_ipv6=1")
-
-    for switch in net.switches:
-        switch.cmd("sysctl -w net.ipv6.conf.all.disable_ipv6=1")
-        switch.cmd("sysctl -w net.ipv6.conf.default.disable_ipv6=1")
-        switch.cmd("sysctl -w net.ipv6.conf.lo.disable_ipv6=1")
 
     net.start()
     CLI(net)
