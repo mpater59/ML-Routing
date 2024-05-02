dfc8460c79ae0628091a741c499edf45c2901ab0
mpater59 <mikolajpater123@gmail.com>
2024-05-02 18:29:52 +0200

    mininet_topo.py fixes

--- mininet_topo.py
+++ mininet_topo.py
@@ -69,7 +69,7 @@ if __name__ == '__main__':
 
     for switch_id in range(1, NUMBER_OF_ROUTERS + 1):
         for host_id in range(1, args.hosts + 1):
-            host = net.get(f's{switch_id}-h{host_id}')
+            host = net.get(f's{switch_id}h{host_id}')
             host.setARP(f'192.168.{10 * switch_id}.1', '00:aa:bb:00:00:01')
             host.setDefaultRoute(f'dev eth0 via 192.168.{10 * switch_id}.1')
 
