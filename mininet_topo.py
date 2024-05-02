1abf679449c2abfd7f91bcdc4c3a7e9b897d263e
mpater59 <mikolajpater123@gmail.com>
2024-05-02 18:01:02 +0200

    mininet_topo.py fixes

--- mininet_topo.py
+++ mininet_topo.py
@@ -19,9 +19,9 @@ class Topology(Topo):
         for x in range(1, NUMBER_OF_ROUTERS + 1):
             routers.append(self.addSwitch(f'r{x}'))
 
-        for x in range(1, NUMBER_OF_ROUTERS + 1):
-            r_switches.append(self.addSwitch(f's{x}'))
-            self.addLink(routers[x-1], r_switches[-1])
+        # for x in range(1, NUMBER_OF_ROUTERS + 1):
+        #     r_switches.append(self.addSwitch(f's{x}'))
+        #     self.addLink(routers[x-1], r_switches[-1])
 
         # hosts = {}
         # for switch_id in range(1, NUMBER_OF_ROUTERS + 1):
@@ -30,20 +30,20 @@ class Topology(Topo):
         #         hosts[switch_id].append(self.addHost(f's{switch_id}-h{host_id}',
         #                                              ip=f'192.168.{10 * switch_id}.{10 * host_id}/24'))
         #         self.addLink(r_switches[switch_id - 1], hosts[switch_id][-1])
-        #
-        # r1 = routers[0]
-        # r2 = routers[1]
-        # r3 = routers[2]
-        # r4 = routers[3]
-        # r5 = routers[4]
-        #
-        # self.addLink(r1, r2, bw=10, delay='10ms')
-        # self.addLink(r1, r3, bw=10, delay='5ms')
-        # self.addLink(r1, r4, bw=15, delay='15ms')
-        # self.addLink(r3, r4, bw=15, delay='15ms')
-        # self.addLink(r4, r2, bw=5, delay='5ms')
-        # self.addLink(r3, r5, bw=5, delay='5ms')
-        # self.addLink(r5, r4, bw=5, delay='5ms')
+
+        r1 = routers[0]
+        r2 = routers[1]
+        r3 = routers[2]
+        r4 = routers[3]
+        r5 = routers[4]
+
+        self.addLink(r1, r2, bw=10, delay='10ms')
+        self.addLink(r1, r3, bw=10, delay='5ms')
+        self.addLink(r1, r4, bw=15, delay='15ms')
+        self.addLink(r3, r4, bw=15, delay='15ms')
+        self.addLink(r4, r2, bw=5, delay='5ms')
+        self.addLink(r3, r5, bw=5, delay='5ms')
+        self.addLink(r5, r4, bw=5, delay='5ms')
 
 
 if __name__ == '__main__':
