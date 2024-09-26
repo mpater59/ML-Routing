d8893f5626b60791d36b18af881e29638de447c9
mpater59 <mikolajpater123@gmail.com>
2024-09-26 22:03:57 +0200

    Modified random_traffic_emulation.py

--- measurements/scripts/random_traffic_emulation.py
+++ measurements/scripts/random_traffic_emulation.py
@@ -4,3 +4,5 @@ def random_traffic_emulation(net, topo_info):
     s1h1.cmdPrint('echo test')
     s1h1.cmd('echo test > test.txt')
     print(s1h1.cmd('cat test.txt'))
+    print(s1h1.cmd('iperf -s'))
+    print('Ended')
