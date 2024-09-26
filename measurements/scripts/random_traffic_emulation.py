e86083b60c6e90cfd62bf1de7254c7f7aecf4f06
mpater59 <mikolajpater123@gmail.com>
2024-09-26 22:36:53 +0200

    Modified random_traffic_emulation.py

--- measurements/scripts/random_traffic_emulation.py
+++ measurements/scripts/random_traffic_emulation.py
@@ -9,9 +9,11 @@ def random_traffic_emulation(net, topo_info):
     s1h1.cmdPrint('echo test')
     s1h1.cmd('echo test > test.txt')
     print(s1h1.cmd('cat test.txt'))
-    threading.Thread(target=run_iperf_server, args=(s1h1,)).start()
+    t1 = threading.Thread(target=run_iperf_server, args=(s1h1,))
+    t1.start()
     time.sleep(2)
-    threading.Thread(target=run_iperf_client, args=(s2h1, '192.168.10.10',)).start()
+    t2 = threading.Thread(target=run_iperf_client, args=(s2h1, '192.168.10.10',))
+    t2.start()
 
 
 def run_iperf_server(host):
@@ -22,6 +24,5 @@ def run_iperf_server(host):
 
 def run_iperf_client(host, dest_ip_addr):
     print('Starting iperf client')
-    print(host.cmd(f'ping {dest_ip_addr} -c 3'))
     host.cmd(f'iperf -c {dest_ip_addr}')
     print('Ended iperf client')
