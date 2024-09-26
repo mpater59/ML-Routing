39f7c4f6e4876238a712a47550a35ee521109d67
mpater59 <mikolajpater123@gmail.com>
2024-09-26 22:00:26 +0200

    Modified random_traffic_emulation.py

--- measurements/scripts/random_traffic_emulation.py
+++ measurements/scripts/random_traffic_emulation.py
@@ -1,3 +1,6 @@
 def random_traffic_emulation(net, topo_info):
     s1h1 = net.get('s1h1')
+    print('Printing')
     s1h1.cmdPrint('echo test')
+    s1h1.cmd('echo test > test.txt')
+    print(s1h1.cmd('cat test.txt'))
