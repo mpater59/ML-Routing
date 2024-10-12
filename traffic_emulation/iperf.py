dd9d56471743daba414710e7aacbe4b25c6908aa
mpater59 <mikolajpater123@gmail.com>
2024-10-12 23:24:19 +0200

    Modified project

--- traffic_emulation/iperf.py
+++ traffic_emulation/iperf.py
@@ -13,17 +13,17 @@ DEFAULT_TIME_INTERVAL = [60, 300]
 def run_iperf_server_tcp(server, client, port, output=None):
     print(f' {client.name} -> {server.name} flow - Starting iperf TCP server; host: {server.name}; port: {port}')
     if output is None:
-        server.pexec(f'iperf -s -p {port}')
+        server.cmd(f'iperf -s -p {port} &')
     else:
-        server.pexec(f'iperf -s -p {port} -y C >> {output}')
+        server.cmd(f'iperf -s -p {port} -y C > {output} &')
 
 
 def run_iperf_server_udp(server, client, port, output=None):
     print(f'{client.name} -> {server.name} flow - Starting iperf UDP server; host: {server.name}; port: {port}')
     if output is None:
-        server.pexec(f'iperf -s -p {port} -u')
+        server.cmd(f'iperf -s -p {port} -u &')
     else:
-        server.pexec(f'iperf -s -p {port} -y C -u >> {output}')
+        server.cmd(f'iperf -s -p {port} -y C -u > {output} &')
 
 
 def run_iperf_client_tcp(server, client, port, dest_ip_addr, bandwidth, flow_time):
@@ -55,13 +55,16 @@ def run_server_thread(server, client, server_id, client_id, l4_proto, output=Non
         port = f'2{server_id}{client_id}'
 
     while True:
-        if l4_proto == 'tcp':
-            run_iperf_server_tcp(server, client, port, output)
-        elif l4_proto == 'udp':
-            run_iperf_server_udp(server, client, port, output)
-        else:
-            print('Unknown L4 protocol!')
-            exit()
+        if server.waiting is False:
+            if l4_proto == 'tcp':
+                run_iperf_server_tcp(server, client, port, output)
+                break
+            elif l4_proto == 'udp':
+                run_iperf_server_udp(server, client, port, output)
+                break
+            else:
+                print('Unknown L4 protocol!')
+                exit()
 
 
 def run_client_thread(server, client, server_id, client_id, l4_proto, bandwidth_interval=None,
